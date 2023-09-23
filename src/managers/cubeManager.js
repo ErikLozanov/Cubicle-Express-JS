f49ccb4ee7db2e5b7e976c4c10331adcdcaadc24
Erik Lozanov <wolfierecordss@gmail.com>
2023-09-23 18:05:33 +0300

    Added cube details page

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,7 +1,24 @@
 const uniqid = require('uniqid');
-const cubes = [];
+const cubes = [
+    {
+        id: '3f3pf0k8nclmw5odud',
+     name: 'Mirror Cube',
+     description: 'Cool Mirror Cube',
+     imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Mirror_Cube_solved.png/1200px-Mirror_Cube_solved.png',
+     difficultyLevel: 4
+    },
+    {
+        id: '327pf0k8nclmw5odud',
+     name: 'Rubik Classic',
+     description: 'Evergreen',
+     imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/6/61/Rubiks_cube_solved.jpg',
+     difficultyLevel: 2
+    }
+];
 
 exports.getAll = () => cubes.slice();
+
+exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
 
 exports.create = (cubeData) => {
 
