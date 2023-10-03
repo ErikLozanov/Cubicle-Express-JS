72b5f1386e2b1cb7006898e91489a06e04af67d9
Erik Lozanov <wolfierecordss@gmail.com>
2023-10-03 13:52:11 +0300

    Added User login

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,4 +1,20 @@
+const bcrypt = require('bcrypt');
+
 const User = require('../models/User');
 
-
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (username, password) => {
+    // TODO: Find user
+    const user = await User.findOne({ username });
+    if(!user) {
+        throw new Error('Cannot find username or password')
+    }
+    // TODO: Validate password
+    const isValid = await bcrypt.compare(password, user.password);
+    if(!isValid) {
+        throw new Error('Cannot find username or password');
+    }
+    // TODO: return user
+    return user;
+};
